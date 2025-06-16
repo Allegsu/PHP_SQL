0c6a536964e87159c7a2360732ebf8d4b41750d5
Allegsu Magana <cheekywithcheeky@gmail.com>
2025-06-17 00:53:53 +0100

    spelling check

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ error_reporting(E_ALL & ~E_DEPRECATED & ~E_WARNING);
 $host = 'localhost';
 $user = 'yor_forger';
 $pass = '1234';
-$dbname = 'crunch';
+$dbname = 'anime';
 
 $conn = new mysqli($host, $user, $pass, $dbname);
 if ($conn->connect_error) {
@@ -222,7 +222,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                     <select name="table_name" id="table_name" required>
                         <?= $tableOptions ?>
                     </select><br><br>
-                        <textarea name="search_query" rows="4" cols="60" placeholder="Search something (e.g., Naruto)..."><?= htmlspecialchars($searchQuery) ?></textarea><br>
+                        <textarea name="search_query" rows="4" cols="60" placeholder="Search something (e.g., Yor Forger)..."><?= htmlspecialchars($searchQuery) ?></textarea><br>
             <button type="submit">
                     <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                         <path d="M12 10V3M13.4141 10.5857L18.3639 5.63599M14 12H21M13.4143 13.4141L18.364 18.3639M12 14V21M10.5859 13.4143L5.63611 18.364M10 12H3M10.5857 10.5859L5.63599 5.63611M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12ZM14 12C14 13.1046 13.1046 14 12 14C10.8954 14 10 13.1046 10 12C10 10.8954 10.8954 10 12 10C13.1046 10 14 10.8954 14 12Z" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
